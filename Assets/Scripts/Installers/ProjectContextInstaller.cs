311fff52575dc37db068e745005ebe51f2e97491
worldDevourer <danyterx123@gmail.com>
2025-04-27 03:35:46 +0200

    project context installer added

---
+++ Assets/Scripts/Installers/ProjectContextInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace Installers
+{
+    public class ProjectContextInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
